0d5c09f938f31b40e113dfc3ccc2fc14f48b960a
venkata penmetcha <vsrpenmetcha@gmail.com>
2018-07-21 16:57:27 -0700

    commiting hello world python program

---
+++ Hello.py
@@ -0,0 +1,28 @@
+import sys
+def main():
+    list = [1, 2, 14, 22, 12, 9]
+    for l in list:
+        print l
+  
+    words = ['cat', 'card', 'apple', 'laptop', 'on']
+    for w in words:
+         print w, len(w)
+
+    pets = ['cat', 'dog', 'parrot', 'butterfly', 'cow']
+    for p in range(len(pets)):
+        print p, pets[p]
+
+               
+if __name__ == ' __main__':
+    main()
+
+    
+
+
+
+
+
+ 
+    
+
+        
